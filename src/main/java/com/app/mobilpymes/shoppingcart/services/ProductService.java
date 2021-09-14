1baf1e0b6cccd6b403cdd963e6c03b1501a1378c
whuera <whuera@gmail.com>
2021-09-14 01:09:16 -0500

    add service layer

---
+++ src/main/java/com/app/mobilpymes/shoppingcart/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.app.mobilpymes.shoppingcart.services;
+
+import com.app.mobilpymes.shoppingcart.entity.Category;
+import com.app.mobilpymes.shoppingcart.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public
+    List < Product > listAllProduct();
+    public Product getProduct(Long id);
+
+    public Product createProduct(Product product);
+    public Product updateProduct(Product product);
+    public  Product deleteProduct(Long id);
+    public List<Product> findByCategory(Category category);
+    public Product updateStock(Long id, Double quantity);
+}
---
+++ src/main/java/com/app/mobilpymes/shoppingcart/services/impl/ProductServiceImpl.java
@@ -0,0 +1,52 @@
+package com.app.mobilpymes.shoppingcart.services.impl;
+
+import com.app.mobilpymes.shoppingcart.entity.Category;
+import com.app.mobilpymes.shoppingcart.entity.Product;
+import com.app.mobilpymes.shoppingcart.services.ProductService;
+
+import java.util.List;
+
+public
+class ProductServiceImpl implements ProductService {
+    @Override
+    public
+    List < Product > listAllProduct ( ) {
+        return null;
+    }
+
+    @Override
+    public
+    Product getProduct (Long id) {
+        return null;
+    }
+
+    @Override
+    public
+    Product createProduct (Product product) {
+        return null;
+    }
+
+    @Override
+    public
+    Product updateProduct (Product product) {
+        return null;
+    }
+
+    @Override
+    public
+    Product deleteProduct (Long id) {
+        return null;
+    }
+
+    @Override
+    public
+    List < Product > findByCategory (Category category) {
+        return null;
+    }
+
+    @Override
+    public
+    Product updateStock (Long id, Double quantity) {
+        return null;
+    }
+}
